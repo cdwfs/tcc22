bed072380d4b658e6dbad11d2218b1e478846456
Cort <1944792+cdwfs@users.noreply.github.com>
2022-12-26 13:38:50 -0800

    Convert day 2 to .lua

---
+++ tcc02.lua
@@ -0,0 +1,54 @@
+t=0
+s=math.sin
+function TIC()
+t=t+1
+cls(9)
+x=120
+k=137
+w=10
+r=1
+o=1
+rect(x-5,x,10,40,3)
+for y=10,x,5 do
+tri(x,y-9,x+w,y,x-w,y,6)
+w=w+2
+if(y>20) then
+circ(x+o,y-9,3,r)
+r=(r+7)%9+1
+o=(o+k)%w-w/2
+end
+end
+for y=1,k do
+pix(99*y%240,(t+y)%k,12)
+rect(4*y,k-s(y)+s(10*y/3)-t/x,4,999,12)
+end
+end
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <TRACKS>
+-- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+-- </TRACKS>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+

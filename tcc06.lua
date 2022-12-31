c75537cfe2544187ebd982d71350cdc4290e92bd
Cort <1944792+cdwfs@users.noreply.github.com>
2022-12-30 22:42:58 -0800

    WIP day 6 317(272)

---
+++ tcc06.lua
@@ -0,0 +1,49 @@
+t=0
+s=math.sin
+X="Tiny Code Christmas!"
+function TIC()
+ t=t+1
+ for y=-68,67 do
+  for x=-120,119 do
+   T=math.atan2(y,x)*2.546-62.8
+   S=299/(x*x+y*y+1)^.5
+   pix(x+120,y+68,T+S-t*.1)
+  end
+	end
+ for j=0,1 do
+ for i=1,#X do
+  print(string.sub(X,i,i),
+  	i*24+250-(t%750)+j,20*s(.1*t+i)+50+j,
+   12*j,0,3)
+ end
+ end
+end
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <TRACKS>
+-- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+-- </TRACKS>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+

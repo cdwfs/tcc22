095f8e33baf4b6f10be3fb0d272ddaf2a759d17f
Cort <1944792+cdwfs@users.noreply.github.com>
2022-12-31 14:48:03 -0800

    WIP day 8 304(266)

---
+++ tcc08.lua
@@ -0,0 +1,48 @@
+t=0
+s=math.sin
+for x=0,240 do
+ rect(x,0,1,140,x)
+ poke(16320+(x%48),s(x/15+s(x%3*13.7))^2*256)
+end
+function TIC()
+ t=t+.01
+ for x=0,238 do
+  for y=0,136 do
+   pix(x,y,pix(x+1,y)-1)
+  end
+ end
+ for T=0,24,5 do
+  circ(169+(20+15*s(t))*s(2*t+T+11),
+   68+(40+20*s(t-11))*s(5*t+T),
+   5+2*s(4*t),12)
+ end
+end
+-- <TILES>
+-- 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
+-- 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
+-- 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
+-- 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+-- 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
+-- 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+-- </TILES>
+
+-- <WAVES>
+-- 000:00000000ffffffff00000000ffffffff
+-- 001:0123456789abcdeffedcba9876543210
+-- 002:0123456789abcdef0123456789abcdef
+-- </WAVES>
+
+-- <SFX>
+-- 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
+-- </SFX>
+
+-- <TRACKS>
+-- 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+-- </TRACKS>
+
+-- <PALETTE>
+-- 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
+-- </PALETTE>
+
